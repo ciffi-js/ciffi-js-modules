8ec35df540b3021bebbfd7d6f0e293fd3b7e9081
ciffi <alessio@ciffi.it>
2018-10-17 00:04:26 +0200

    [cookies] - start refactoring

--- __tests__/Cookies.test.js
+++ __tests__/Cookies.test.js
@@ -1,5 +1,6 @@
-import Cookies from '../packages/Cookies/Cookies';
+import Cookies, { checkExpire } from '../packages/Cookies/Cookies';
 
-test('Cookies', () => {
-  expect(1).toBe(1);
+test('Cookies check expire date', () => {
+  const date = checkExpire('data da parsare');
+  expect(date).toBe('expire=datatrasformata');
 });
